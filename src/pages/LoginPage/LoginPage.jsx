1d8ff1196c59a6593f03cd3ad5e31c9d094f0631
srlsrx <nicolasin97@gmail.com>
2025-05-22 10:47:57 +0200

    feat: login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,1 +1,21 @@
-import React from "react";
+import React from 'react'
+import { ListChecks } from 'lucide-react'
+
+const LoginPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+        <ListChecks size={54} className='mb-4.5' />
+        <h1>Taskify</h1>
+        <div className='flex flex-col items-center justify-center'>
+            <h2 className='text-2xl font-bold'>Login</h2>
+            <form className='flex flex-col items-center'>
+                <input type="text" placeholder="Username" className='border border-gray-300 rounded p-2 mb-4' />
+                <input type="password" placeholder="Password" className='border border-gray-300 rounded p-2 mb-4' />
+                <button type="submit" className='bg-blue-500 text-white rounded p-2'>Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
